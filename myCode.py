c5d5753bc46c7cbc68e516ae23002c4db555a8b6
Mark Thackham <mark.thackham@gmail.com>
2024-05-18 18:04:56 +0100

    Commit again

---
+++ myCode.py
@@ -0,0 +1,14 @@
+from datetime import date, timedelta
+
+class AdvanedAnalytics:
+  def __init__(self, ):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+#Use the Person class to create an object, and then execute the printname method:
+
+x = Person("John", "Doe")
+x.printname()
