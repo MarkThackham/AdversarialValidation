32f6e2384f6fc578b1062bb7c1ce07e679b178e3
Mark Thackham <mark.thackham@gmail.com>
2024-05-18 18:16:47 +0100

    c

--- myCode.py
+++ myCode.py
@@ -2,8 +2,8 @@ from datetime import date, timedelta
 
 class AdvanedAnalytics:
     def __init__(self, input1):
-        self.startDate = input1
+        self.input = input1
 
     def last_day(self):
-        temp=self.startDate.replace(day=1) + timedelta(days=31)
+        temp=self.input.replace(day=1) + timedelta(days=31)
         return temp-timedelta(days=temp.day)
