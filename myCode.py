8c5dc458156eb06f2c86786fd25d38014a2b6f66
Mark Thackham <mark.thackham@gmail.com>
2024-05-18 18:10:47 +0100

    commit

--- myCode.py
+++ myCode.py
@@ -1,14 +1,9 @@
 from datetime import date, timedelta
 
 class AdvanedAnalytics:
-  def __init__(self, ):
-    self.firstname = fname
-    self.lastname = lname
+    def __init__(self, input1):
+        self.startDate = input1
 
-  def printname(self):
-    print(self.firstname, self.lastname)
-
-#Use the Person class to create an object, and then execute the printname method:
-
-x = Person("John", "Doe")
-x.printname()
+    def last_day(self):
+        temp=self.startDate.replace(day=1) + timedelta(days=31)
+        return temp-timedelta(days=temp.day)
