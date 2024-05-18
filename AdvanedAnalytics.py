c76c890992fa84bad47d4d7031bf36649195e151
Mark Thackham <mark.thackham@gmail.com>
2024-05-18 19:43:41 +0100

    Commit

--- AdvanedAnalytics.py
+++ AdvanedAnalytics.py
@@ -1,9 +1,27 @@
 from datetime import date, timedelta
 
 class AdvanedAnalytics:
+    # WoE encoding
+    # Target encoding
+    # IV
+    # Correlation
+    # VIF
+    # Adversarial Validati0n
+    # RFECV
+    # Optuna
+    # FLAML
+
+
+
     def __init__(self):
         self._a=None
     
     def last_day(input1):
+        # Utility to return the last day of the month
         temp=input1.replace(day=1) + timedelta(days=31)
         return temp-timedelta(days=temp.day)
+    
+    def first_day(input1):
+        # Utility to return the first day of the month
+        temp=input1.replace(day=1)
+        return temp
